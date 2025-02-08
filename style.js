53b5a54c84844804817a8700689ef13acf5a187a
Haradhan Adhikary <h.adhikary@uw.edu.pl>
2025-02-09 00:07:59 +0100

    Create style.js
    Signed-off-by: Haradhan Adhikary <h.adhikary@uw.edu.pl>

---
+++ style.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    fetch("navbar.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("navbar").innerHTML = data;
+        });
+
+    fetch("footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer").innerHTML = data;
+        });
+
+    mermaid.initialize({
+        startOnLoad: true
+    });
+});
